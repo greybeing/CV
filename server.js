09dd1d831a1dd201b88f7ceeaa3959929511f6d5
greybeing <adulojugbenga@yahoo.com>
2022-11-29 08:50:30 +0100

    edit

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
 const dotenv = require("dotenv");
-const static = require('node-static')
+const static = require('node-static');
+const path = require("path");
 dotenv.config();
 const cors = require('cors');
 
@@ -21,46 +22,84 @@ var corsOptions = {
  app.use(cors(corsOptions)); 
 
 // Data parsing
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(express.urlencoded({ extended: false }));
+
 
 //middleware
 app.use(express.json())
 // app.use(express.static(__dirname + '/public'));
 
 //send email
-app.post('/', (req, res) => {
-    console.log(req.body)
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        service: 'gmail',
-        port: 587,
-        secure: true,
-        auth: {
-            user:process.env.USER,
-            pass:process.env.PASSWORD
-        },
-        debug: true
+// app.post('/', (req, res) => {
+//     console.log(req.body)
+//     const transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         service: 'gmail',
+//         port: 587,
+//         secure: true,
+//         auth: {
+//             user:process.env.USER,
+//             pass:process.env.PASSWORD
+//         },
+//         debug: true
+//     });
+
+async function mainMail(name, email, subject, message) {
+    const transporter = await nodeMail.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.PASSWORD,
+      },
     });
 
-const mailOptions = {
-    from: req.body.contactName,
-    to: process.env.USER,
-    subject: `Message from ${req.body.email}: ${req.body.subject}`,
-    text: req.body.message
+// const mailOptions = {
+//     from: req.body.contactName,
+//     to: process.env.USER,
+//     subject: `Message from ${req.body.email}: ${req.body.subject}`,
+//     text: req.body.message
+// }
+
+
+const mailOption = {
+    from: process.env.GMAIL_USER,
+    to: process.env.EMAIL,
+    subject: subject,
+    html: `You got a message from 
+    Email : ${email}
+    Name: ${name}
+    Message: ${message}`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    return Promise.resolve("Message Sent Successfully!");
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
-transporter.sendMail(mailOptions, (error, info) => {
-    if(error) {
-        console.log(error);
-        res.send('error');
-    }else {
-        console.log('Email sent: ' + info.response);
-        res.send('success')
+// transporter.sendMail(mailOptions, (error, info) => {
+//     if(error) {
+//         console.log(error);
+//         res.send('error');
+//     }else {
+//         console.log('Email sent: ' + info.response);
+//         res.send('success')
+//     }
+//  })
+
+
+  app.post("/", async (req, res, next) => {
+    const { contactName, contactEmail, contactSubject, contactMessage } = req.body;
+    try {
+      await mainMail(contactName, contactEmail, contactSubject, contactMessage );
+      
+      res.send("Message Successfully Sent!");
+    } catch (error) {
+      res.send("Message Could not be Sent");
     }
- })
-});
+  });
+ 
 
 // setup static file server
 const fileServer = new(static.Server)('./public');
@@ -69,3 +108,4 @@ require('http').createServer(function (request, response) {
         fileServer.serve(request, response);
     }).resume();
 }).listen(process.env.PORT, () => console.log(`server is running on Port ${process.env.PORT}`))
+
